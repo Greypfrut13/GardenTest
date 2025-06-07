678ca898d8722a54234c02408558db922a134247
Igor Ryazanov <123439439+Greypfrut13@users.noreply.github.com>
2025-06-07 15:23:09 +0800

    Created player movement

---
+++ Assets/Scripts/Player/PlayerCore.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Player
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class PlayerCore : MonoBehaviour
+    {
+        [FormerlySerializedAs("_playerMovement")]
+        [Header("References")]
+        [SerializeField] private PlayerMovement _movement;
+
+        private void Awake()
+        {
+            _movement.Init(this);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        [SerializeField] private float _movementSpeed;
+        [SerializeField] private Joystick _joystick;
+        
+        private Rigidbody2D _rigidbody;
+
+        public void Init(PlayerCore core)
+        {
+            _rigidbody = core.GetComponent<Rigidbody2D>();
+        }
+
+        private void FixedUpdate()
+        {
+            Move();
+        }
+
+        private void Move()
+        {
+            Vector2 direction = _joystick.Direction;
+            _rigidbody.velocity = direction * _movementSpeed;
+        }
+    }
+}
