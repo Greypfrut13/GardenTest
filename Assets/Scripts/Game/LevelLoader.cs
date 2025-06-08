0c09b7fbf2459f99ecfe70221ddabc093771a368
Igor Ryazanov <123439439+Greypfrut13@users.noreply.github.com>
2025-06-08 22:07:31 +0800

    Created GameOver screen and Level Loader

---
+++ Assets/Scripts/Game/LevelLoader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Game
+{
+    public class LevelLoader : MonoBehaviour
+    {
+        [SerializeField] private Button _restartButton;
+
+        private void Start()
+        {
+            _restartButton.onClick.AddListener(RestartLevel);
+        }
+
+        public void RestartLevel()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
