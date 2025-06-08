5ccbda6ad55f45058a0c9ba6d999170c89767043
Igor Ryazanov <123439439+Greypfrut13@users.noreply.github.com>
2025-06-08 14:25:15 +0800

    Created Enemy Health

--- Assets/Scripts/Enemies/EnemyHealth.cs
+++ Assets/Scripts/Enemies/EnemyHealth.cs
@@ -1,12 +1,49 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Enemies
 {
     public class EnemyHealth : MonoBehaviour
     {
+        [Header("Settings")]
+        [SerializeField] [Min(0.0f)] private float _maxHealth;
+        
+        [Header("UI")]
+        [SerializeField] private Slider _healthSlider;
+        
+        private float _currentHealth;
+
+        private void Start()
+        {
+            _currentHealth = _maxHealth; 
+            
+            _healthSlider.maxValue = _maxHealth;
+            _healthSlider.value = _currentHealth;
+            _healthSlider.gameObject.SetActive(false);
+
+        }
+
         public void TakeDamage(float damage)
         {
-            return;
+            _currentHealth -= damage;
+            UpdateHealthUI();
+            
+            if (_currentHealth <= 0)
+            {
+                Die();
+            }
+        }
+
+        private void UpdateHealthUI()
+        {
+            _healthSlider.gameObject.SetActive(true);
+            _healthSlider.value = _currentHealth;
+        }
+        
+        private void Die()
+        {
+            Destroy(gameObject);
         }
     }
 }
