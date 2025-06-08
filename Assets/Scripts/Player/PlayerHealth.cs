1a9d7f6710787f423a778baf166d7e6b53742ad1
Igor Ryazanov <123439439+Greypfrut13@users.noreply.github.com>
2025-06-08 21:56:08 +0800

    Created Player Health

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,12 +1,48 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Player
 {
     public class PlayerHealth : MonoBehaviour
     {
+        [Header("Settings")]
+        [SerializeField] [Min(0.0f)] private float _maxHealth;
+        
+        [Header("UI")]
+        [SerializeField] private Slider _healthSlider;
+        [SerializeField] private GameObject _deathScreen;
+        
+        private float _currentHealth;
+
+        private void Start()
+        {
+            _currentHealth = _maxHealth;
+            UpdateHealthUI();
+            _deathScreen.SetActive(false);
+        }
+
         public void TakeDamage(float damage)
         {
-            Debug.Log("Damaged");
+            _currentHealth -= damage;
+            UpdateHealthUI();
+
+            if (_currentHealth <= 0)
+            {
+                Die();
+            }
+        }
+
+        private void UpdateHealthUI()
+        {
+            _healthSlider.value = _currentHealth / _maxHealth;
+        }
+
+        private void Die()
+        {
+            Time.timeScale = 0;
+            _deathScreen.SetActive(true);
+            Destroy(gameObject);
         }
     }
 }
