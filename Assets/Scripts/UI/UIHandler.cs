d27267b3c16d32508de316306c02f6edd4f94820
Igor Ryazanov <123439439+Greypfrut13@users.noreply.github.com>
2025-06-09 13:16:50 +0800

    Created open and close button for Inventory

---
+++ Assets/Scripts/UI/UIHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class UIHandler : MonoBehaviour
+    {
+        [Header("Inventory")]
+        [SerializeField] private Button _inventoryButton;
+        [SerializeField] private GameObject _inventoryPanel;
+
+        private void Awake()
+        {
+            _inventoryButton.onClick.AddListener(OpenAndCloseInventory);
+        }
+
+        public void OpenAndCloseInventory()
+        {
+            if (_inventoryPanel.activeInHierarchy)
+            {
+                _inventoryPanel.SetActive(false);
+                Time.timeScale = 1;
+            }
+            else
+            {
+                _inventoryPanel.SetActive(true);
+                Time.timeScale = 0;
+            }
+        }
+    }
+}
